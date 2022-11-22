6178b0d202e79dd5d43413b43351e3892fd7a279
stefan <stefanallen10@gmail.com>
2022-11-22 11:59:45 +0000

    co450
    co450

---
+++ Stefan-Logbook/LogBook2(1)/RazorCurrencyConverter/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorCurrencyConverter.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string FullName { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        #pragma warning disable CS8618
+        public IndexModel(ILogger<IndexModel> logger)
+        #pragma warning restore CS8618
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            FullName = "Anonymous";
+        }
+        public void OnPost()
+        {
+            if (string.IsNullOrEmpty(FullName))
+            {
+                ViewData["Message"] = "You have not entered a name!";
+            }
+            else
+            {
+                ViewData["Message"] = "Account is Registered!";
+            }
+        }
+    }
+}
