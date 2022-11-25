c084636abfe4b3864f51211182b4329c6d3d5c80
stefan <stefanallen10@gmail.com>
2022-11-25 15:33:52 +0000

    lb2(2)

---
+++ Stefan-Logbook/LogBook2(2)/RazorCurrencyConverter/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+
+namespace RazorCurrencyConverter.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+
+        public string? x { get; set; }
+        public const double y = 1.21;
+        double result;
+
+        public void OnPost()
+        {
+            if (!String.IsNullOrWhiteSpace(x))
+            {
+                result = Convert.ToDouble(x) * y;
+                ViewData["Message"] = $"£{x} Is ${result}";
+            }
+
+        }
+    }
+}
